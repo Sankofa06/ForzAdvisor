f2da3b375bdc6de99b782d49fff30307c0993ff9
Michael <Williams>
2026-05-20 00:00:56 -0700

    Initial Commit

---
+++ forzadvisor/forzadvisorApp.swift
@@ -0,0 +1,17 @@
+//
+//  forzadvisorApp.swift
+//  forzadvisor
+//
+//  Created by Michael Williams on 5/20/26.
+//
+
+import SwiftUI
+
+@main
+struct forzadvisorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
